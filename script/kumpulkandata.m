58b856d353ad15d4f40d863922f307a78d4a12da
miftanurfarid <miftanurfarid@gmail.com>
2016-08-05 21:31:01 +0700

    remove unused script

--- script/kumpulkandata.m
+++ script/kumpulkandata.m
@@ -36,62 +36,63 @@ for T=1:5;
             masker=masker(1:length(target),:);
         end
 
-        TL=conv(target,hrtf_target_left);
-        TR=conv(target,hrtf_target_right);
+%        TL=conv(target,hrtf_target_left);
+%        TR=conv(target,hrtf_target_right);
         ML=conv(masker,hrtf_masker_left);
         MR=conv(masker,hrtf_masker_right);
-        MIXL=TL+ML;
-        MIXR=TR+MR;
+%        MIXL=TL+ML; %data training tanpa suara campur
+%        MIXR=TR+MR;
 
         %% gammatone filterbank
 
-        gfTL=gammatone(TL,128,[80,5000],16000);
-        gfTR=gammatone(TR,128,[80,5000],16000);
+%        gfTL=gammatone(TL,128,[80,5000],16000);
+%        gfTR=gammatone(TR,128,[80,5000],16000);
         gfML=gammatone(ML,128,[80,5000],16000);
         gfMR=gammatone(MR,128,[80,5000],16000);
-        gfMIXL=gammatone(MIXL,128,[80,5000],16000);
-        gfMIXR=gammatone(MIXR,128,[80,5000],16000);
+%        gfMIXL=gammatone(MIXL,128,[80,5000],16000); %data training tanpa suara campur
+%        gfMIXR=gammatone(MIXR,128,[80,5000],16000);
 
         %% hair cell filter
 
-        hcTL=meddis(gfTL,16000);
-        hcTR=meddis(gfTR,16000);
+%        hcTL=meddis(gfTL,16000);
+%        hcTR=meddis(gfTR,16000);
         hcML=meddis(gfML,16000);
         hcMR=meddis(gfMR,16000);
-        hcMIXL=meddis(gfMIXL,16000);
-        hcMIXR=meddis(gfMIXR,16000);
+%        hcMIXL=meddis(gfMIXL,16000); %data training tanpa suara campur
+%        hcMIXR=meddis(gfMIXR,16000);
 
         %% cochleagram
+        % hilangkan tanda % untuk melakukan plot cochleagram
 
-        cgTL=cochleagram(hcTL,320);
-        cgTR=cochleagram(hcTR,320);
-        cgML=cochleagram(hcML,320);
-        cgMR=cochleagram(hcMR,320);
-        cgMIXL=cochleagram(hcMIXL,320);
-        cgMIXR=cochleagram(hcMIXR,320);
+%        cgTL=cochleagram(hcTL,320);
+%        cgTR=cochleagram(hcTR,320);
+%        cgML=cochleagram(hcML,320);
+%        cgMR=cochleagram(hcMR,320);
+%        cgMIXL=cochleagram(hcMIXL,320);
+%        cgMIXR=cochleagram(hcMIXR,320);
 
         %% itd
 %         [itd_T,corrT]=meancrosscorrelogram(hcTL,hcTR,-3500,3500,16000);
-        itdchanframeT=itdchanframe(hcTL,hcTR);
+%        itdchanframeT=itdchanframe(hcTL,hcTR);
 %         itdaverageT=averageitd(corrT);
 %         [itd_M,corrM]=meancrosscorrelogram(hcML,hcMR,-3500,3500,16000);
         itdchanframeM=itdchanframe(hcML,hcMR);
 %         itdaverageM=averageitd(corrM);
 %         [itd_MIX,corrMIX]=meancrosscorrelogram(hcMIXL,hcMIXR,-3500,3500,16000);
-        itdchanframeMIX=itdchanframe(hcMIXL,hcMIXR);
+%        itdchanframeMIX=itdchanframe(hcMIXL,hcMIXR);
 %         itdaverageMIX=averageitd(corrMIX);
 
         %% ild
-        ildchanframeT = ildchanframe(hcTL,hcTR);
+%        ildchanframeT = ildchanframe(hcTL,hcTR);
 %         ild_T= meanild(hcTL,hcTR);
         ildchanframeM = ildchanframe(hcML,hcMR);
 %         ild_M= meanild(hcML,hcMR);
-        ildchanframeMIX = ildchanframe(hcMIXL,hcMIXR);
+%        ildchanframeMIX = ildchanframe(hcMIXL,hcMIXR);
 %         ild_MIX= meanild(hcMIXL,hcMIXR);
 
         %% ratio
         ratioL=ratiochanframe(hcTL,hcML);
-        ratioR=ratiochanframe(hcTR,hcMR);
+%        ratioR=ratiochanframe(hcTR,hcMR);
         
         %% save all data
 %         nama_data=strcat('dataT',int2str(T),'N',int2str(N));
